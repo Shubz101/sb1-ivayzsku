17188d4bb4c20b884edaa71e00fdeeec28244cc8
Shubz101 <shubzzz1019@gmail.com>
2024-12-15 17:08:53 +0530

    Update GameMenu.tsx

--- src/components/GameMenu.tsx
+++ src/components/GameMenu.tsx
@@ -17,7 +17,7 @@ const games: GameInfo[] = [
   {
     id: 'tictactoe',
     title: 'Tic Tac Toe',
-    description: 'The classic game of X's and O's',
+    description: 'The classic game of Xs and Os',
     icon: Gamepad2,
     color: 'from-blue-500 to-blue-600'
   },
